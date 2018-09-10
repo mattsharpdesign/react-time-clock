4e11c78a40573e24436061b957e77a5eb10f4212
Matt Sharp <mattsharpdesign@gmail.com>
2018-09-11 07:22:01 +1200

    Add profile placeholder image

--- src/EmployeeCard.js
+++ src/EmployeeCard.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { Card, Image } from 'semantic-ui-react';
+import placeholder from './profile_placeholder.png';
 
 class EmployeeCard extends Component {
   state = {  }
   render() { 
     const { employee, onSelect } = this.props;
+    const profilePicUrl = employee.profilePicUrl || placeholder;
     return (
       <Card onClick={() => onSelect(employee)}>
-        <Image src={employee.profilePicUrl} />
+        <Image src={profilePicUrl} />
         <Card.Content>
           <Card.Header>{employee.lastName}, {employee.firstName}</Card.Header>
           <Card.Meta>Status...</Card.Meta>
