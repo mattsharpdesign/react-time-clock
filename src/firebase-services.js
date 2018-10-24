10adef0ae72ddc34e5a0584f54f4fbe7a2fa6896
matt <matt@mattsharp.co.nz>
2018-10-24 17:38:17 +1300

    Set up hosting for test env

--- src/firebase-services.js
+++ src/firebase-services.js
@@ -4,13 +4,14 @@ import 'firebase/firestore';
 import 'firebase/storage';
 
 var config = {
-  apiKey: "AIzaSyDSsYkAs3468LoL3b2GkcF2EsM2uyzvpeg",
-  authDomain: "onlinetimekeeper.firebaseapp.com",
-  databaseURL: "https://onlinetimekeeper.firebaseio.com",
-  projectId: "onlinetimekeeper",
-  storageBucket: "onlinetimekeeper.appspot.com",
-  messagingSenderId: "298832097505"
+  apiKey: "AIzaSyAYCaOQw0gDHySugvZOBZFe-eImvt9z9cQ",
+  authDomain: "timeclock-testing.firebaseapp.com",
+  databaseURL: "https://timeclock-testing.firebaseio.com",
+  projectId: "timeclock-testing",
+  storageBucket: "timeclock-testing.appspot.com",
+  messagingSenderId: "170289627623"
 };
+
 console.log('Initializing Firebase app');
 firebase.initializeApp(config);
 
